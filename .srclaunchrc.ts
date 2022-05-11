7a31560105c0ebba5c8e20291f82e82ecf4b6a93
Steven Bennett <steven@budgetbloom.com>
2022-05-10 19:11:39 -0700

    chore: Clean installation cache

---
+++ .srclaunchrc.ts
@@ -0,0 +1,56 @@
+import {
+  BuildFormat,
+  BuildPlatform,
+  BuildTarget,
+  BuildTool,
+  CodeFormatterTool,
+  CodeLinterTool,
+  StaticTypingTool,
+  Project,
+  ProjectType,
+  PublishAccess,
+  TestReporter,
+  License,
+  TestTool,
+} from "@srclaunch/types";
+
+export default <Project>{
+  name: "@srclaunch/web-environment",
+  description: "Web environment specific utilities for SrcLaunch applications",
+  type: ProjectType.Library,
+  build: {
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Node,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+  test: {
+    coverage: {
+      reporters: [TestReporter.Lcov, TestReporter.JSONSummary],
+    },
+    tool: TestTool.Ava,
+  },
+  environments: {
+    development: {
+      formatters: [CodeFormatterTool.Prettier],
+      linters: [CodeLinterTool.ESLint],
+      staticTyping: [StaticTypingTool.TypeScript],
+    },
+  },
+  release: {
+    publish: {
+      access: PublishAccess.Public,
+      license: License.MIT,
+      registry: "https://registry.npmjs.org/",
+    },
+  },
+  requirements: {
+    node: ">=16",
+    yarn: ">=3.2.0",
+    srclaunch: {
+      dx: true,
+      cli: true,
+      types: true,
+    },
+  },
+};
